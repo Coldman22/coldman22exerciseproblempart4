10f2c673e59368604c76f4b582120c19c8a60bc4
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-30 18:45:54 +0530

    Add files via upload

---
+++ sdusingfunction.cpp
@@ -0,0 +1,38 @@
+//calculate Standard Deviation Using Function.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double calculate_sd(double data[]);
+
+int main(){
+    int i;
+    double data[10];
+    cout<<"enter 10 elements: ";
+
+    for ( i = 0; i < 10; i++)
+    {
+        cin>> data[i];
+        cout<<endl<<"SD: "<<calculate_sd(data);
+
+        return 0;
+    }
+    
+}
+
+double calculate_sd(double data[]){
+    double sum =0.0, mean, sd =0.0;
+    int i;
+    
+    for ( i = 0; i < 10; i++)
+    {
+        sum += data[i];
+    }
+    mean = sum /10;
+
+    for ( i = 0; i < 10; i++)
+    {
+        sd += pow(data[i] - mean,2);
+    }
+    return sqrt(sd/10);
+}
